fa3a8a1ea89c74b0670347385c555d5c544dd02f
Jalil <Jalilsan@gmail.com>
2014-07-21 14:20:54 -0700

    youtube api working and also seeded Video model with sample videos

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 gem 'minitest'
+gem 'youtube_it'
 group :test do
   gem 'rspec-rails', '~>3.0.0'
   gem 'shoulda-matchers'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'youtube_it'
+API_KEY = "AIzaSyCJQy1o0qwdB7LzwZ5gmLiSUUap6nfBpxw"
+client = YouTubeIt::Client.new(:dev_key => API_KEY)
+video = client.videos_by(:query => "penguin", :per_page => 1)
+Video.create(:title => video.videos[0].title, :url => video.videos[0].player_url, :image => video.videos[0].thumbnails[0].url)
+
