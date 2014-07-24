b8eadc1e24d3fdc77c3c041612b5fe8a63ff3f89
Jalil <Jalilsan@gmail.com>
2014-07-23 23:33:12 -0700

    changed query size to five videos only

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,10 @@
 require 'youtube_it'
 API_KEY = "AIzaSyCJQy1o0qwdB7LzwZ5gmLiSUUap6nfBpxw"
 client = YouTubeIt::Client.new(:dev_key => API_KEY)
-video = client.videos_by(:query => "summer makeup tutorial", :per_page => 6)
+video = client.videos_by(:query => "summer makeup tutorial", :per_page => 5)
+Video.destroy_all
 Category.create(name: "Summer makeup")
-1.upto(5) do |i|
-  video.videos.each do |vid|
-    Video.create(category_id: 1, :title => vid.title, :url => vid.player_url, :image => vid.thumbnails[i].url)
-  end
+video.videos.each_with_index do |vid, num|
+  Video.create(category_id: 1, :title => vid.title, :url => vid.player_url, :image => vid.thumbnails[num].url)
 end
 
